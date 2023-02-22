412182c76e75133f62e4bb20ec1751a4a89cf109
13232160100 <ypc0100@qq.com>
2023-02-22 22:16:35 +0800

    更新样式

--- src/components/FaceTrack.vue
+++ src/components/FaceTrack.vue
@@ -36,10 +36,10 @@ function f(path) {
   <div style="text-align: center">
     <a>手机在QQ浏览器测试</a>
     <div>
-      <button @click="f('/p1')">图案一</button>
-      <button @click="f('/p2')">图案二</button>
-      <button @click="f('/p3')">图案三</button>
-      <button @click="f('/p4')">图案四</button>
+      <button class="btn btn-primary" @click="f('/p1')">图案一</button>&thinsp;
+      <button class="btn btn-primary" @click="f('/p2')">图案二</button>&thinsp;
+      <button class="btn btn-primary" @click="f('/p3')">图案三</button>&thinsp;
+      <button class="btn btn-primary" @click="f('/p4')">图案四</button>
     </div>
     <div id="container"></div>
   </div>
@@ -55,5 +55,33 @@ body {
   height: 100vh;
   position: relative;
   overflow: hidden;
+}
+
+.btn {
+  display: inline-block;
+  margin-bottom: 0;
+  font-weight: normal;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  -ms-touch-action: manipulation;
+  touch-action: manipulation;
+  cursor: pointer;
+  background-image: none;
+  border: 1px solid transparent;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  border-radius: 4px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.btn-primary {
+  color: #fff;
+  background-color: #337ab7;
+  border-color: #2e6da4;
 }
 </style>
